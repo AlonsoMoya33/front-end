04020a8e13c28b761a1679006106f48c8f911744
Vanessa-coder <idaliaflores788@gmail.com>
2022-06-03 09:39:28 -0600

    delete lines
    Delete component Contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentesComponent } from './componentes/componentes.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { ServicesComponent } from './componentes/services/services.component';
 import { LogInComponent } from './componentes/log-in/log-in.component';
@@ -16,7 +14,6 @@ import { SignUpComponent } from './componentes/sign-up/sign-up.component';
     AppComponent,
     ComponentesComponent,
     NavbarComponent,
-    ContactoComponent,
     HomeComponent,
     ServicesComponent,
     LogInComponent,
